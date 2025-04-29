eac1b51e9fb792fcbc608c60f10519ae597d4d6e
Edoardo Comini <167285074+EdoardoComini@users.noreply.github.com>
2025-04-29 16:49:08 +0200

    first method for the creation of id's and implementation of it in the user class

--- byPass/src/main/GestoreLogin.java
+++ byPass/src/main/GestoreLogin.java
@@ -1,5 +1,26 @@
 package main;
 
-public class GestoreLogin {
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.Random;
 
+public class GestoreLogin {
+public static ArrayList<Integer> idUsati;
+    public ArrayList<Utente> listaUtenti;
+public Date dataAttuale;
+
+public GestoreLogin(Date dataAttuale){
+    this.dataAttuale=dataAttuale;
+    this.listaUtenti=new ArrayList<>();
+}
+public static int generaId(){
+    Random rand =new Random();
+    int nuovoId=0;
+    do{nuovoId=rand.nextInt(10000);
+}while(idUsati.contains(nuovoId));
+    idUsati.add(nuovoId);
+    return nuovoId;
+
+
+} 
 }
--- byPass/src/main/Utente.java
+++ byPass/src/main/Utente.java
@@ -7,17 +7,16 @@ public class Utente {
 public int id;
 public String nome, email, password;
 public Date dataReg;
-public static ArrayList<Integer> idGi‡Usati;
 
 public Utente(int id, String nome, String email, String password, Date dataReg) {
-	if(idGi‡Usati.contains(id)) {
-		
-	}
-	this.nome = nome;
+	this.id=GestoreLogin.generaId();
+    this.nome = nome;
 	this.email=email;
 	this.password=password;
 	this.dataReg=new Date(System.currentTimeMillis());
-	
 }
-
+@Override
+public String toString(){
+	return" Utente "+id+"[nome: "+nome+" email"+email+"]";
+}
 }
